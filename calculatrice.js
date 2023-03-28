e7a38a4052cc781b85fb4b8fe9764b15272492d3
Maxence De Poorter <maxence.depoorter.pro@gmail.com>
2023-03-28 14:11:05 +0200

    étape 2
    Ajout de la classe calculatrice
    Bouton retour en arrière déjà ajouté à l'étape 1
    Ajout d'une fonctionnalité affichant erreur si le résultat n'est pas calculable au lieu de ne rien faire
    Modification du HTML pour permettre le fonctionnement avec la classe

--- calculatrice.js
+++ calculatrice.js
@@ -1,22 +1,34 @@
-//Fichier contenant toutes les fonctions nécessaires aux fonctionnement de la calculatrice
+class Calculator {
+  set(nb) {
+    const calcScreenEqual = document.getElementById('calcScreenEqual');
+    const calcScreenResult = document.getElementById('calcScreenResult');
+    calcScreenEqual.value += nb;
+  }
 
-//Fonction permettant d'afficher le nombre sur l'écran de la calculatrice
-function set(nb) {
-    document.getElementById('calcScreenEqual').value += nb;
-}
+  equal() {
+    const expression = document.getElementById('calcScreenEqual').value;
+    const isCalculable = /^[\d+\-*/().\s]+$/.test(expression); // Vérifie si l'expression contient uniquement des caractères autorisés
 
-//Fonction permettant d'afficher le résulat sur l'écran de la calculatrice
-function equal(){
-    document.getElementById('calcScreenResult').value = eval(document.getElementById('calcScreenEqual').value);
-}
+    if (isCalculable) {
+      try {
+        const result = eval(expression);
+        document.getElementById('calcScreenResult').value = result;
+      } catch (error) {
+        document.getElementById('calcScreenResult').value = 'Erreur';
+      }
+    } else {
+      document.getElementById('calcScreenResult').value = 'Erreur';
+    }
+  }
 
-//Fonction permettant de supprimer le contenu des écrans de la calculatrice
-function clr(){
+  clr() {
     document.getElementById('calcScreenEqual').value = "";
     document.getElementById('calcScreenResult').value = "";
+  }
+
+  del() {
+    document.getElementById('calcScreenEqual').value = document.getElementById('calcScreenEqual').value.slice(0, -1);
+  }
 }
 
-//Fonction permettant de supprimer le dernier caractère de l'écran de la calculatrice
-function del(){
-    document.getElementById('calcScreenEqual').value = document.getElementById('calcScreenEqual').value.slice(0, -1);
-}
+const calc = new Calculator();
